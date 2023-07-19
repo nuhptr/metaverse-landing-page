8f548bd7996652c0d8c243b96aea9cbd107860d5
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-07-19 10:51:28 +0700

    explore page

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,18 @@
-'use client';
+"use client";
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from "framer-motion";
+import { textContainer, textVariant2 } from "../utils/motion";
+
+export const TypingText = ({ title, textStyles }) => (
+   <motion.p
+      variants={textContainer}
+      className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
+      {Array.from(title).map((letter, index) => (
+         <motion.span variants={textVariant2} key={index}>
+            {letter === "" ? "\u00A0" : letter}
+         </motion.span>
+      ))}
+   </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,9 +1,12 @@
-'use client';
+"use client";
 
-const Explore = () => (
-  <section>
-    Explore section
-  </section>
-);
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+import { TypingText } from "../components";
+
+const Explore = () => <section>Explore section</section>;
 
 export default Explore;
