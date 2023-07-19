a675eeccc73ac3a380a3491b230cd32c0af509b5
nuhptr <nugrahaadi733@gmail.com>
2023-07-20 01:30:12 +0700

    finish the whatsnew section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,7 +1,17 @@
-const NewFeatures = () => (
-  <div>
-    New Features
-  </div>
-);
+import styles from "../styles";
 
-export default NewFeatures;
+export default function NewFeatures({ imgUrl, title, subtitle }) {
+   return (
+      <div className='flex flex-col flex-1 sm:max-w-[250px] min-w-[210px]'>
+         <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
+            <img src={imgUrl} alt='Icon' className='object-contain w-1/2 h-1/2' />
+         </div>
+         <h1 className='mt-[26px] font-black text-[24px] leading-[30px] text-white'>
+            Title {title}
+         </h1>
+         <p className='flex-1 mt-[16px] font-normal text-[18px] text-[#b0b0b0] leading-[32px]'>
+            {subtitle}
+         </p>
+      </div>
+   );
+}
