d005ad6c893dbca36ca3b5a586ac09339f588ec5
nuhptr <nugrahaadi733@gmail.com>
2023-09-18 19:20:52 +0700

    app: refactoring

--- app/head.js
+++ app/head.js
@@ -1,11 +1,11 @@
 export default function Head() {
-   return (
-      <>
-         <title>Metaversus</title>
-         <meta name='viewport' content='width=device-width, initial-scale=1' />
-         <link rel='icon' href='/favicon.ico' />
-         <link rel='preconnect' href='https://stijndv.com' />
-         <link rel='stylesheet' href='https://stijndv.com/fonts/Eudoxus-Sans.css' />
-      </>
-   );
+  return (
+    <>
+      <title>Metaversus</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='preconnect' href='https://stijndv.com' />
+      <link rel='stylesheet' href='https://stijndv.com/fonts/Eudoxus-Sans.css' />
+    </>
+  )
 }
--- app/page.js
+++ app/page.js
@@ -1,33 +1,38 @@
-import { Footer, Navbar } from "../components";
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from "../sections";
+import { Footers, Navbar } from '../components'
+import { About, Explores, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections'
 
 export const metadata = {
-   title: "Metaversus",
-   description: "Metaversus is a metaverse that is built on top of the Ethereum blockchain.",
-};
+  title: 'Metaversus',
+  description: 'Metaversus is a metaverse that is built on top of the Ethereum blockchain.',
+}
 
 export default function Page() {
-   return (
-      <div className='overflow-hidden bg-primary-black'>
-         <Navbar />
-         <Hero />
-         <div className='relative'>
-            <About />
-            <div className='z-0 gradient-03' />
-            <Explore />
-         </div>
-         <div className='relative'>
-            <GetStarted />
-            <div className='z-0 gradient-04' />
-            <WhatsNew />
-         </div>
-         <World />
-         <div className='relative'>
-            <Insights />
-            <div className='z-0 gradient-05' />
-            <Feedback />
-         </div>
-         <Footer />
+  return (
+    <div className='overflow-hidden bg-primary-black'>
+      <Navbar />
+      <Hero />
+
+      <div className='relative'>
+        <About />
+        <div className='z-0 gradient-03' />
+        <Explores />
       </div>
-   );
+
+      <div className='relative'>
+        <GetStarted />
+        <div className='z-0 gradient-04' />
+        <WhatsNew />
+      </div>
+
+      <World />
+
+      <div className='relative'>
+        <Insights />
+        <div className='z-0 gradient-05' />
+        <Feedback />
+      </div>
+
+      <Footers />
+    </div>
+  )
 }
