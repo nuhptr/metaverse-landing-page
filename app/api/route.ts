ed5f735382c9d1b548f6ccb68536743204d93495
nuhptr <nugrahaadi733@gmail.com>
2023-09-29 11:12:26 +0700

    delete route.ts

--- app/api/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'This is metaverse API' })
-}
